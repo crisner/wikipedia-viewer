7020cd9ba73147263ae5ea00ad3ed8fed6ac20df
crisner <christie.reni@gmail.com>
2018-02-02 12:06:50 +0530

    basic functionality done

--- js/index.js
+++ js/index.js
@@ -1,44 +1,74 @@
-
-    
-    // $("#submit").on("click", function(e) {
-    //     searchUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&list=backlinks&titles=" + searchText + "&rvprop=content&rvlimit=max&bltitle=" + searchText + "&bllimit=10";
-        
-    //     // $("#search").attr("action","$.getJSON(searchUrl, displayResults)");
-    //     // $("main").html(json.limits.revisions);
-    //     // console.log(searchText, searchUrl);
-    //     // event.preventDefault();
-    // });
-    
     function displayResults() { 
         var searchText = "";
         var searchUrl;
         var results;
         $("#search").on("change", function() {
             searchText = this.value;
-            searchUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=revisions&list=backlinks&titles=" + searchText + "&rvprop=content&rvlimit=max&bltitle=" + searchText + "&bllimit=10";
-            // console.log(searchText, searchUrl);
+            searchUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + searchText + "&limit=max&profile=fuzzy";
         });
         
         $("#submit").on("click", function(e) {
             $.getJSON(searchUrl, getResults);
-            // console.log("link",searchText, searchUrl);
             event.preventDefault();
         });
+
+        $("#random").on("click", function(e) {
+            window.open("https://en.wikipedia.org/wiki/Special:Random");
+        });
+
+        // $(window).on("scroll", function(e) {
+        //     $.getJSON(searchUrl, loadMore);
+        //     // event.preventDefault();
+        // });
     }
 
     function getResults(json) {
+        var links = json;
+        var title = json[1];
+        // var length = title.length;
+        var length = 10;
+        var description = json[2];
+        var url = json[3];
         var link = "<ul>";
-        
-           var links = json.query.backlinks;
-        links.forEach(function(val) {
-            link += "<li>" + val.title + "</li>";
 
-        });
-        // link = json.backlinks[0];
-        link += "</ul>"
-            console.log(link);
-            $("main").html(link);
+        for (var i = 0; i < length; i++) {
+            link += "<li><a target='_blank' href='" + url[i] + "'>" + title[i] + "</a><p>" + description[i] + "</p></li>"
+        };
+        // if ($(window).scrollTop() + $(window).height() > $("main").height()) {
+        //     for (var i = ; i < length; i++) {
+        //         link += "<li><a target='_blank' href='" + url[i] + "'>" + title[i] + "</a><p>" + description[i] + "</p></li>"
+        //     }
+        //     $("main").append(link); 
+        //   }
         
-        
+        link += "</ul>";
+            
+        $("main").html(link);
+        // .show("slow", function() {
+        //     return length < 5;
+        // });    
     }
+
+    // function loadMore(json) {
+    //     var links = json;
+    //     var title = json[1];
+    //     // var length = title.length;
+    //     var length = 10;
+    //     var description = json[2];
+    //     var url = json[3];
+    //     var link = "<ul>";
+
+    //     if ($(window).scrollTop() + $(window).height() > $("main").height()) {
+    //         for (var i = ; i < length; i++) {
+    //             link += "<li><a target='_blank' href='" + url[i] + "'>" + title[i] + "</a><p>" + description[i] + "</p></li>"
+    //         }
+    //         $("main").append(link); 
+    //       }
+        
+    //     link += "</ul>";
+            
+         
+        
+    // }
+
 displayResults();
